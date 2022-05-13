cd2015915f95255afacf6b98654c0cd8f4cdf786
Iraikare Rodrigues <>
2022-05-13 18:36:55 +0000

    inseri arquivo bem vindo

---
+++ 13.05.22/bemvindo.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Digite seu nome");
+    // s = input()
+    string s = Console.ReadLine();
+    Console.WriteLine("Bem vindo(a), " + s);  
+  }
+}
